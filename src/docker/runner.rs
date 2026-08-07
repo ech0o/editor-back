7c2ee12210c8d3cad437e889bd274435474d39a8
ech0o <asdfghjklcx1250@outlook.com>
2026-08-07 19:57:18 +0800

    add: code exec

--- src/docker/runner.rs
+++ src/docker/runner.rs
@@ -1,29 +1,36 @@
+use std::collections::HashMap;
+use std::fs::remove_dir_all;
+use std::path::Path;
 use std::sync::Arc;
 
+use crate::docker::exec::ExecResult;
+use crate::docker::image::pull_if_needed;
+use crate::docker::workspace::{create_workspace, write_source};
+use crate::workspace::Workspace;
 use anyhow::Result;
-use bollard::{
-    Docker,
-    models::ContainerCreateBody,
-    query_parameters::CreateContainerOptionsBuilder,
-};
 use bollard::container::LogOutput;
 use bollard::exec::{CreateExecOptions, StartExecResults};
-use bollard::query_parameters::{RemoveContainerOptionsBuilder, StartContainerOptions};
-use uuid::Uuid;
-use crate::docker::image::pull_if_needed;
+use bollard::models::HostConfig;
+use bollard::query_parameters::{
+    ListContainersOptionsBuilder, RemoveContainerOptionsBuilder, StartContainerOptions,
+};
+use bollard::{
+    Docker, models::ContainerCreateBody, query_parameters::CreateContainerOptionsBuilder,
+};
 use futures_util::StreamExt;
 use serde::Serialize;
+use uuid::Uuid;
 
 #[derive(Clone)]
 pub struct DockerRunner {
     docker: Arc<Docker>,
 }
 
-#[derive(Serialize)]
-pub struct ExecResult {
-    pub stdout: String,
-    pub stderr: String,
-    pub exit_code: i64,
+#[derive(Serialize, Debug)]
+pub struct RunResponse {
+    stdout: String,
+    stderr: String,
+    exit_code: i64,
 }
 impl DockerRunner {
     pub fn new(docker: Arc<Docker>) -> Self {
@@ -34,33 +41,26 @@ impl DockerRunner {
         self.docker.as_ref()
     }
 
-    async fn pull_if_needed(
-        &self,
-        image: &str,
-    ) -> Result<()> {
-        pull_if_needed(
-            self.docker(),
-            image,
-        )
-            .await
+    async fn pull_if_needed(&self, image: &str) -> Result<()> {
+        pull_if_needed(self.docker(), image).await
     }
 
-    pub async fn create(
-        &self,
-        image: &str,
-    ) -> Result<String> {
+    pub async fn create(&self, image: &str, workspace: &Path) -> Result<String> {
         self.pull_if_needed(image).await?;
+
+        let host_config = HostConfig {
+            binds: Some(vec![format!("{}:/workspace", workspace.to_string_lossy())]),
+            ..Default::default()
+        };
 
         let config = ContainerCreateBody {
             image: Some(image.to_owned()),
-            cmd: Some(vec![
-                "sleep".into(),
-                "3600".into(),
-            ]),
+            cmd: Some(vec!["sleep".into(), "3600".into()]),
             tty: Some(false),
             attach_stdout: Some(true),
             attach_stderr: Some(true),
             open_stdin: Some(false),
+            host_config: Some(host_config),
             ..Default::default()
         };
 
@@ -68,60 +68,126 @@ impl DockerRunner {
             .name(&format!("runner-{}", Uuid::new_v4()))
             .build();
 
-        let response = self
-            .docker
-            .create_container(Some(options), config)
-            .await?;
+        let response = self.docker.create_container(Some(options), config).await?;
 
         Ok(response.id)
     }
 
-    pub async fn start(&self,id:&str) -> Result<()> {
-        self.docker.start_container(id,None::<StartContainerOptions>).await?;
+    pub async fn start(&self, id: &str) -> Result<()> {
+        self.docker
+            .start_container(id, None::<StartContainerOptions>)
+            .await?;
         Ok(())
     }
 
-    pub async fn remove(&self,id:&str) -> Result<()> {
-        let options=RemoveContainerOptionsBuilder::default().force(true).build();
-        self.docker.remove_container(id,Some(options)).await?;
+    pub async fn remove(&self, id: &str) -> Result<()> {
+        let options = RemoveContainerOptionsBuilder::default().force(true).build();
+        self.docker.remove_container(id, Some(options)).await?;
         Ok(())
     }
 
-    pub async fn exec(&self,id:&str,cmd:&Vec<String>)->anyhow::Result<ExecResult>{
-        let mut ret = ExecResult{
-            stdout: "".to_string(),
-            stderr: "".to_string(),
-            exit_code: 0,
-        };
-        let exec=self.docker.create_exec(
-            id,
-            CreateExecOptions{
-                attach_stdout:Some(true),
-                attach_stderr:Some(true),
-                cmd:Some(cmd.clone()),
-                ..Default::default()
-            }
-        ).await?;
-        let output=self.docker.start_exec(&exec.id, None).await?;
-        match output {
-            StartExecResults::Attached {mut output ,.. } => {
-                while let Some(msg) = output.next().await {
-                    println!("{:?}",msg);
-                    match msg? {
-                        LogOutput::StdOut {message} => {
-                            ret.stdout.push_str(&String::from_utf8_lossy(&message));
-                        },
-                        LogOutput::StdErr {message} => {
-                            ret.stderr.push_str(&String::from_utf8_lossy(&message));
-                        }
-                        _=>{}
-                    }
+    pub async fn exec(&self, id: &str, cmd: Vec<String>) -> anyhow::Result<ExecResult> {
+        crate::docker::exec::exec(self.docker(), id, cmd).await
+    }
+
+    pub async fn run_rust(&self, code: &str) -> anyhow::Result<RunResponse> {
+        let workspace = Workspace::new()?;
+        workspace.write("main.rs", code)?;
+        self.with_container("rust:1.89",workspace.path(),|id|async move{
+            let compile = self
+                .exec(
+                    &id,
+                    vec![
+                        "rustc".into(),
+                        "/workspace/main.rs".into(),
+                        "-o".into(),
+                        "/workspace/main".into(),
+                    ],
+                )
+                .await?;
+            let result = if compile.exit_code != 0 {
+                RunResponse {
+                    stdout: String::new(),
+                    stderr: compile.stderr,
+                    exit_code: compile.exit_code,
                 }
-            }
-            StartExecResults::Detached => {}
-        }
+            } else {
+                let res = self.exec(&id, vec!["/workspace/main".into()]).await?;
+                RunResponse {
+                    stdout: res.stdout,
+                    stderr: res.stderr,
+                    exit_code: res.exit_code,
+                }
+            };
+            Ok(result)
+        }).await
+        // let id = self.create("rust:1.89", workspace.path()).await?;
+        // self.start(id.as_str()).await?;
+        // let compile = self
+        //     .exec(
+        //         &id,
+        //         vec![
+        //             "rustc".into(),
+        //             "/workspace/main.rs".into(),
+        //             "-o".into(),
+        //             "/workspace/main".into(),
+        //         ],
+        //     )
+        //     .await?;
+        // let result = if compile.exit_code != 0 {
+        //     RunResponse {
+        //         stdout: String::new(),
+        //         stderr: compile.stderr,
+        //         exit_code: compile.exit_code,
+        //     }
+        // } else {
+        //     let res = self.exec(&id, vec!["/workspace/main".into()]).await?;
+        //     RunResponse {
+        //         stdout: res.stdout,
+        //         stderr: res.stderr,
+        //         exit_code: res.exit_code,
+        //     }
+        // };
+        // self.remove(&id).await?;
+        // Ok(result)
+    }
 
-        let inspect=self.docker.inspect_exec(&exec.id).await?;
-        Ok(ret)
+    async fn cleanup_container(&self, id: &str) -> Result<()> {
+        self.remove(id).await?;
+        Ok(())
+    }
+
+    pub async fn with_container<F, Fut, T>(&self, image: &str, workspace: &Path, f: F) -> Result<T>
+    where
+        F: FnOnce(String) -> Fut,
+        Fut: Future<Output = anyhow::Result<T>>,
+    {
+        let id = self.create(image, workspace).await?;
+        self.start(id.as_str()).await?;
+        let result = f(id.clone()).await;
+        if let Err(e)=self.cleanup_container(id.as_str()).await{
+            tracing::warn!(container_id = %id,
+                error = %e,
+                "failed to remove container")
+        }
+        result
+    }
+}
+mod test {
+    use super::*;
+    #[tokio::test]
+    async fn test_run() -> anyhow::Result<()> {
+        let docker = Docker::connect_with_local_defaults()?;
+        let runner = DockerRunner::new(Arc::new(docker));
+        let code = r#"
+        fn main() {
+            println!("Hello, Runner!");
+        }
+        "#;
+
+        let result = runner.run_rust(code).await?;
+
+        println!("{:#?}", result);
+        Ok(())
     }
 }
