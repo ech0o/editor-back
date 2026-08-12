cd0702116d91c3334e562f9b54b3892753d751f7
ech0o <asdfghjklcx1250@outlook.com>
2026-08-12 14:47:10 +0800

    add: api errors

---
+++ src/apierror.rs
@@ -0,0 +1,55 @@
+use axum::{
+    Json,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+use serde::{Deserialize, Serialize};
+
+pub enum ApiError {
+    Internal(anyhow::Error),
+    InvalidJson,
+    PayloadTooLarge,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ErrorResponse {
+    pub error: String,
+}
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        match self {
+            ApiError::Internal(err) => {
+                tracing::error!(
+                    error = %err,
+                    "internal server error"
+                );
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Json(ErrorResponse {
+                        error: String::from("internal server error"),
+                    }),
+                )
+                    .into_response()
+            }
+            ApiError::InvalidJson => (
+                StatusCode::BAD_REQUEST,
+                Json(ErrorResponse {
+                    error: String::from("invalid_json"),
+                }),
+            )
+                .into_response(),
+            ApiError::PayloadTooLarge => (
+                StatusCode::PAYLOAD_TOO_LARGE,
+                Json(ErrorResponse {
+                    error: String::from("payload_too_large"),
+                })
+                ).into_response(),
+        }
+    }
+}
+
+impl From<anyhow::Error> for ApiError {
+    fn from(e: anyhow::Error) -> Self {
+        Self::Internal(e)
+    }
+}
