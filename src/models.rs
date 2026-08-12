50a0978cf3c3e8f7e5689614772d49dcb25f2ae1
ech0o <asdfghjklcx1250@outlook.com>
2026-08-12 14:42:46 +0800

    add: run exec status

--- src/models.rs
+++ src/models.rs
@@ -1,17 +1,30 @@
-use serde::{
-    Deserialize,
-    Serialize,
-};
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Deserialize,Serialize,Debug)]
 pub struct RunRequest {
-    pub language: String,
+    // pub language: String,
     pub code: String,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum RunStatus {
+    Accepted,
+    CompileError,
+    RuntimeError,
+    TimeLimitExceeded,
+    MemoryLimitExceeded,
+}
+#[derive(Debug, Serialize, Deserialize)]
 pub struct RunResponse {
     pub stdout: String,
+    pub status: RunStatus,
     pub stderr: String,
     pub exit_code: i64,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ExecResult {
+    pub stdout: String,
+    pub stderr: String,
+    pub exit_code: Option<i64>,
 }
