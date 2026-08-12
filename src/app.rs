3f045cd0bbb9b41822fdda3972f98a262e63e7de
ech0o <asdfghjklcx1250@outlook.com>
2026-08-12 14:46:56 +0800

    add: cors,tests

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,137 @@
-use axum::Router;
-use bollard::Docker;
-
+use crate::apierror::ErrorResponse;
+use crate::models::{RunRequest, RunResponse, RunStatus};
 use crate::{routes, state::AppState};
+use axum::Router;
+use axum::http::{HeaderValue, Method};
+use axum::{
+    body::Body,
+    http::{Request, StatusCode},
+};
+use bollard::Docker;
+use tower::ServiceExt;
+use tower_http::cors::{Any, CorsLayer};
+use tower_http::limit::RequestBodyLimitLayer;
 
 pub async fn create_app() -> anyhow::Result<Router> {
     let docker = Docker::connect_with_local_defaults()?;
 
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>()?)
+        .allow_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_headers(Any);
+
     Ok(Router::new()
+        .layer(RequestBodyLimitLayer::new(1024 * 1024))
+        .layer(cors)
         .merge(routes::router())
         .with_state(AppState::new(docker)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::{RunResponse, RunStatus};
+    use axum::http::Method;
+
+    #[tokio::test]
+    async fn post_run_returns_accepted() {
+        let app = create_app().await.unwrap();
+        let request = Request::builder()
+            .method(Method::POST)
+            .uri("/run")
+            .header("content-type", "application/json")
+            .body(Body::from(
+                r#"{
+                    {"code":"// Type your code here\nfn main(){\n    println!(\"hello\");\n}"}
+                }"#,
+            ))
+            .unwrap();
+        let response = app.oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = axum::body::to_bytes(response.into_body(), usize::MAX)
+            .await
+            .unwrap();
+        let res: RunResponse = serde_json::from_slice(&body).expect("invalid RunResponse");
+        assert!(matches!(res.status, RunStatus::Accepted));
+        assert_eq!(res.exit_code, 0);
+        assert!(res.stdout.contains("hello"));
+    }
+}
+
+#[tokio::test]
+async fn post_run_compile_error() {
+    let app = create_app().await.unwrap();
+    let request = Request::builder()
+        .method(Method::POST)
+        .uri("/run")
+        .header("content-type", "application/json")
+        .body(Body::from(
+            r#"{
+                "code": "fn main() { let x: i32 = \"hello\"; }"
+            }"#,
+        ))
+        .unwrap();
+
+    let response = app.oneshot(request).await.unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = axum::body::to_bytes(response.into_body(), usize::MAX)
+        .await
+        .unwrap();
+    let res: RunResponse = serde_json::from_slice(&body).expect("invalid RunResponse");
+    assert!(matches!(res.status, RunStatus::CompileError));
+    assert_ne!(res.exit_code, 0);
+}
+
+#[tokio::test]
+async fn post_run_rejects_invalid_json() {
+    let app = create_app().await.unwrap();
+    let request = Request::builder()
+        .method(Method::POST)
+        .uri("/run")
+        .header("content-type", "application/json")
+        .body(Body::from(r#"this is not json"#))
+        .unwrap();
+    let response = app.oneshot(request).await.unwrap();
+    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    let body = axum::body::to_bytes(response.into_body(), usize::MAX)
+        .await
+        .unwrap();
+
+    let error: ErrorResponse = serde_json::from_slice(&body).unwrap();
+
+    assert_eq!(error.error, "invalid_json");
+}
+
+#[tokio::test]
+async fn post_run_rejects_missing_code() {
+    let app = create_app().await.unwrap();
+
+    let request = Request::builder()
+        .method("POST")
+        .uri("/run")
+        .header("content-type", "application/json")
+        .body(Body::from(r#"{"foo":"bar"}"#))
+        .unwrap();
+
+    let response = app.oneshot(request).await.unwrap();
+
+    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+}
+
+#[tokio::test]
+async fn post_run_rejects_large_body() {
+    let app = create_app().await.unwrap();
+    let code = "a".repeat(2*1024*1024);
+    let body = serde_json::to_string(&RunRequest { code }).unwrap();
+    let request = Request::builder()
+        .method(Method::POST)
+        .uri("/run")
+        .header("content-type", "application/json")
+        .body(Body::from(body))
+        .unwrap();
+    let response = app.oneshot(request).await.unwrap();
+    // let body = axum::body::to_bytes(response.into_body(), usize::MAX).await.unwrap();
+    // println!("{:?}", body);
+    assert_eq!(response.status(), StatusCode::PAYLOAD_TOO_LARGE);
 }
