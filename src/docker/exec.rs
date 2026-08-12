f976541a8b4092e5a34988f0c9b9e99ddfc87930
ech0o <asdfghjklcx1250@outlook.com>
2026-08-12 14:45:46 +0800

    refactor: stdout & stderr vec

--- src/docker/exec.rs
+++ src/docker/exec.rs
@@ -7,41 +7,42 @@ use bollard::{
 
 use futures_util::StreamExt;
 use serde::Serialize;
-
-#[derive(Serialize)]
-pub struct ExecResult {
-    pub stdout: String,
-    pub stderr: String,
-    pub exit_code: i64,
-}
+pub(crate) use crate::models::ExecResult;
+// #[derive(Serialize)]
+// pub struct ExecResult {
+//     pub stdout: String,
+//     pub stderr: String,
+//     pub exit_code: i64,
+// }
 
 pub async fn exec(docker: &Docker, container_id: &str, cmd: Vec<String>) -> Result<ExecResult> {
+    let config = CreateExecOptions {
+        attach_stdout: Some(true),
+        attach_stderr: Some(true),
+        cmd: Some(cmd),
+        ..Default::default()
+    };
     let exec = docker
         .create_exec(
             container_id,
-            CreateExecOptions {
-                attach_stdout: Some(true),
-                attach_stderr: Some(true),
-                cmd: Some(cmd),
-                ..Default::default()
-            },
+            config
         )
         .await?;
     let res = docker
         .start_exec(&exec.id, None::<StartExecOptions>)
         .await?;
-    let mut stdout = String::new();
-    let mut stderr = String::new();
+    let mut stdout = Vec::new();
+    let mut stderr = Vec::new();
     match res {
         StartExecResults::Attached { mut output, .. } => {
             while let Some(item) = output.next().await {
                 let item = item?;
                 match item {
                     LogOutput::StdOut { message } => {
-                        stdout.push_str(&String::from_utf8_lossy(&message));
+                        stdout.extend_from_slice(&message);
                     }
                     LogOutput::StdErr { message } => {
-                        stderr.push_str(&String::from_utf8_lossy(&message));
+                        stderr.extend_from_slice(&message);
                     }
                     _ => {}
                 }
@@ -52,8 +53,8 @@ pub async fn exec(docker: &Docker, container_id: &str, cmd: Vec<String>) -> Resu
     let inspect = docker.inspect_exec(&exec.id).await?;
     let exit_code = inspect.exit_code.unwrap_or(-1);
     Ok(ExecResult {
-        stdout,
-        stderr,
-        exit_code,
+        stdout:String::from_utf8_lossy(&stdout).to_string(),
+        stderr:String::from_utf8_lossy(&stderr).to_string(),
+        exit_code:Some(exit_code),
     })
 }
