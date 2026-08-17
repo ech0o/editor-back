174e8cd6d73148e7a7f39e75bc1cddc98569911e
ech0o <asdfghjklcx1250@outlook.com>
2026-08-17 20:39:38 +0800

    add: graceful_shutdown

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,28 @@ mod state;
 mod workspace;
 mod apierror;
 mod error;
+mod job;
+mod kafka;
+mod db;
 
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-
+    dotenvy::dotenv()?;
     let app = app::create_app().await?;
 
     let listener = TcpListener::bind("0.0.0.0:4000").await?;
     tracing::info!("Listening on http://0.0.0.0:4000");
 
-    axum::serve(listener, app).await?;
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
 
     Ok(())
+}
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c().await.expect("failed to install CTRL+C signal handler");
 }
