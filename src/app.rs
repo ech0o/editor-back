b5abb042a5bd41fac75d360006ed7af9f0665244
ech0o <asdfghjklcx1250@outlook.com>
2026-08-17 20:42:37 +0800

    add: kafka

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,10 @@
 use crate::apierror::ErrorResponse;
+use crate::db::Database;
+use crate::docker::DockerRunner;
+use crate::job::{Job, JobStore};
+use crate::kafka::{KafkaConsumer, KafkaProducer};
 use crate::models::{RunRequest, RunResponse, RunStatus};
+use crate::state::JobState;
 use crate::{routes, state::AppState};
 use axum::Router;
 use axum::http::{HeaderValue, Method};
@@ -8,80 +13,115 @@ use axum::{
     http::{Request, StatusCode},
 };
 use bollard::Docker;
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::RwLock;
 use tower::ServiceExt;
 use tower_http::cors::{Any, CorsLayer};
 use tower_http::limit::RequestBodyLimitLayer;
 
 pub async fn create_app() -> anyhow::Result<Router> {
     let docker = Docker::connect_with_local_defaults()?;
-
+    let runner = DockerRunner::new(Arc::new(docker));
+    let db = Database::new("sqlite://judge.db").await?;
+    let kafka = KafkaProducer::new("localhost:9092")?;
+    // let (job_tx, job_rx) = tokio::sync::mpsc::channel::<Job>(100);
+    let jobs = Arc::new(JobStore::new(db.pool));
     let cors = CorsLayer::new()
         .allow_origin("http://localhost:3000".parse::<HeaderValue>()?)
         .allow_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE])
         .allow_headers(Any);
-
+    // jobs.migrate().await?;
+    let consumer = KafkaConsumer::new("localhost:9092", "judge-worker", runner, jobs.clone())?;
+    consumer.subscribe()?;
+    tokio::spawn(async move {
+        if let Err(error) = consumer.run().await {
+            eprintln!("Error consuming Kafka messages: {}", error);
+        }
+    });
     Ok(Router::new()
         .layer(RequestBodyLimitLayer::new(1024 * 1024))
         .layer(cors)
         .merge(routes::router())
-        .with_state(AppState::new(docker)))
+        .with_state(AppState::new(jobs, kafka)))
 }
+
+// async fn worker(
+//     mut rx: tokio::sync::mpsc::Receiver<Job>,
+//     runner: DockerRunner,
+//     jobs: Arc<JobStore>,
+// ) {
+//     while let Some(job) = rx.recv().await {
+//         jobs.set_running(job.id).await;
+//         println!("received job {:?}", job.id);
+//         let result = runner.run_rust(job.id.clone(), &job.code).await;
+//         match result {
+//             Ok(run_res) => {
+//                 jobs.finish(job.id, RunStatus::Success, Some(run_res)).await;
+//             }
+//             Err(_) => {
+//                 jobs.finish(job.id, RunStatus::RuntimeError, None).await;
+//             }
+//         };
+//         // println!("job {} finished with result {:?}", job.id, result);
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::models::{RunResponse, RunStatus};
     use axum::http::Method;
-
-    #[tokio::test]
-    async fn post_run_returns_accepted() {
-        let app = create_app().await.unwrap();
-        let request = Request::builder()
-            .method(Method::POST)
-            .uri("/run")
-            .header("content-type", "application/json")
-            .body(Body::from(
-                r#"{
-                    {"code":"// Type your code here\nfn main(){\n    println!(\"hello\");\n}"}
-                }"#,
-            ))
-            .unwrap();
-        let response = app.oneshot(request).await.unwrap();
-        assert_eq!(response.status(), StatusCode::OK);
-
-        let body = axum::body::to_bytes(response.into_body(), usize::MAX)
-            .await
-            .unwrap();
-        let res: RunResponse = serde_json::from_slice(&body).expect("invalid RunResponse");
-        assert!(matches!(res.status, RunStatus::Accepted));
-        assert_eq!(res.exit_code, 0);
-        assert!(res.stdout.contains("hello"));
-    }
+    //
+    // #[tokio::test]
+    // async fn post_run_returns_accepted() {
+    //     let app = create_app().await.unwrap();
+    //     let request = Request::builder()
+    //         .method(Method::POST)
+    //         .uri("/run")
+    //         .header("content-type", "application/json")
+    //         .body(Body::from(
+    //             r#"{
+    //                 {"code":"// Type your code here\nfn main(){\n    println!(\"hello\");\n}"}
+    //             }"#,
+    //         ))
+    //         .unwrap();
+    //     let response = app.oneshot(request).await.unwrap();
+    //     assert_eq!(response.status(), StatusCode::OK);
+    //
+    //     let body = axum::body::to_bytes(response.into_body(), usize::MAX)
+    //         .await
+    //         .unwrap();
+    //     let res: RunResponse = serde_json::from_slice(&body).expect("invalid RunResponse");
+    //     assert!(matches!(res.status, RunStatus::Accepted));
+    //     assert_eq!(res.exit_code, 0);
+    //     assert!(res.stdout.contains("hello"));
+    // }
 }
 
-#[tokio::test]
-async fn post_run_compile_error() {
-    let app = create_app().await.unwrap();
-    let request = Request::builder()
-        .method(Method::POST)
-        .uri("/run")
-        .header("content-type", "application/json")
-        .body(Body::from(
-            r#"{
-                "code": "fn main() { let x: i32 = \"hello\"; }"
-            }"#,
-        ))
-        .unwrap();
-
-    let response = app.oneshot(request).await.unwrap();
-    assert_eq!(response.status(), StatusCode::OK);
-    let body = axum::body::to_bytes(response.into_body(), usize::MAX)
-        .await
-        .unwrap();
-    let res: RunResponse = serde_json::from_slice(&body).expect("invalid RunResponse");
-    assert!(matches!(res.status, RunStatus::CompileError));
-    assert_ne!(res.exit_code, 0);
-}
+// #[tokio::test]
+// async fn post_run_compile_error() {
+//     let app = create_app().await.unwrap();
+//     let request = Request::builder()
+//         .method(Method::POST)
+//         .uri("/run")
+//         .header("content-type", "application/json")
+//         .body(Body::from(
+//             r#"{
+//                 "code": "fn main() { let x: i32 = \"hello\"; }"
+//             }"#,
+//         ))
+//         .unwrap();
+//
+//     let response = app.oneshot(request).await.unwrap();
+//     assert_eq!(response.status(), StatusCode::OK);
+//     let body = axum::body::to_bytes(response.into_body(), usize::MAX)
+//         .await
+//         .unwrap();
+//     let res: RunResponse = serde_json::from_slice(&body).expect("invalid RunResponse");
+//     assert!(matches!(res.status, RunStatus::CompileError));
+//     assert_ne!(res.exit_code, 0);
+// }
 
 #[tokio::test]
 async fn post_run_rejects_invalid_json() {
@@ -122,7 +162,7 @@ async fn post_run_rejects_missing_code() {
 #[tokio::test]
 async fn post_run_rejects_large_body() {
     let app = create_app().await.unwrap();
-    let code = "a".repeat(2*1024*1024);
+    let code = "a".repeat(2 * 1024 * 1024);
     let body = serde_json::to_string(&RunRequest { code }).unwrap();
     let request = Request::builder()
         .method(Method::POST)
