089769f8e6d02cba89af185eebec582a56dbff13
ech0o <asdfghjklcx1250@outlook.com>
2026-08-17 20:40:24 +0800

    add: sqlite db

--- src/routes.rs
+++ src/routes.rs
@@ -1,37 +1,41 @@
 use crate::apierror::ApiError;
 use crate::docker::exec::ExecResult;
 use crate::docker::workspace::{create_workspace, write_source};
-use crate::models::{RunRequest, RunResponse};
-use crate::state::AppState;
+use crate::job::Job;
+use crate::models::{JobIdResponse, JobMessage, JobResponse, RunRequest, RunResponse, RunStatus};
+use crate::state::{AppState, JobState};
 use axum::error_handling::HandleError;
+use axum::extract::Path;
 use axum::extract::rejection::JsonRejection;
 use axum::http::StatusCode;
 use axum::routing::post;
 use axum::{Json, Router, extract::State, routing::get};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use std::sync::Arc;
+use uuid::Uuid;
 
 pub fn router() -> Router<AppState> {
     Router::new()
-        .route("/health", get(health))
-        // .route("/container", post(create_container))
-        // .route("/exec", get(exec))
-        .route("/mount", get(workspace))
+        // .route("/health", get(health))
+        // // .route("/container", post(create_container))
+        // // .route("/exec", get(exec))
+        // .route("/mount", get(workspace))
         .route("/run", post(run_handle))
+        .route("/run/{job_id}", get(get_job))
 }
 
 async fn handle_err(err: anyhow::Error) -> (StatusCode, String) {
     (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
-async fn health(State(state): State<AppState>) -> Json<serde_json::Value> {
-    let version = state.runner.docker().version().await.unwrap();
-
-    Json(json!({
-        "version": version.version,
-        "api": version.api_version
-    }))
-}
-
+// async fn health(State(state): State<AppState>) -> Json<serde_json::Value> {
+//     let version = state.runner.docker().version().await.unwrap();
+//
+//     Json(json!({
+//         "version": version.version,
+//         "api": version.api_version
+//     }))
+// }
 #[derive(Serialize)]
 struct CreateResponse {
     id: String,
@@ -72,32 +76,32 @@ struct CreateResponse {
 //     Json(result)
 // }
 
-async fn workspace(State(state): State<AppState>) -> Json<ExecResult> {
-    let workspace = create_workspace().unwrap();
-    let _ = write_source(&workspace, "hello.txt", "Hello Docker!");
-    let id = state
-        .runner
-        .create("ubuntu:latest", &workspace)
-        .await
-        .unwrap();
-    let _ = state.runner.start(&id).await;
-    let res = state
-        .runner
-        .exec(&id, vec!["cat".into(), "/workspace/hello.txt".into()])
-        .await;
-    Json(res.unwrap())
-}
+// async fn workspace(State(state): State<AppState>) -> Json<ExecResult> {
+//     let workspace = create_workspace().unwrap();
+//     let _ = write_source(&workspace, "hello.txt", "Hello Docker!");
+//     let id = state
+//         .runner
+//         .create("ubuntu:latest", &workspace)
+//         .await
+//         .unwrap();
+//     let _ = state.runner.start(&id).await;
+//     let res = state
+//         .runner
+//         .exec(&id, vec!["cat".into(), "/workspace/hello.txt".into()])
+//         .await;
+//     Json(res.unwrap())
+// }
 
 async fn run_handle(
     State(state): State<AppState>,
     results: Result<Json<RunRequest>, JsonRejection>,
-) -> Result<Json<RunResponse>, ApiError> {
-    let _permit = match state.semaphore.try_acquire(){
-        Ok(permit) => permit,
-        Err(_)=>{
-            return Err(ApiError::TooManyRequests)
-        }
-    };
+) -> Result<Json<JobIdResponse>, ApiError> {
+    // let _permit = match state.semaphore.try_acquire(){
+    //     Ok(permit) => permit,
+    //     Err(_)=>{
+    //         return Err(ApiError::TooManyRequests)
+    //     }
+    // };
     let Json(request) = results.map_err(|err| match err.status() {
         StatusCode::PAYLOAD_TOO_LARGE => {
             tracing::error!(
@@ -115,7 +119,30 @@ async fn run_handle(
             ApiError::InvalidJson
         }
     })?;
-    tracing::debug!("received request: {:?}", request);
-    let response = state.runner.run_rust(&request.code).await?;
-    Ok(Json(response))
+    // tracing::debug!("received request: {:?}", request);
+    // let response = state.runner.run_rust(&request.code).await?;
+    // Ok(Json(response))
+    let job = Job {
+        id: Uuid::new_v4(),
+        language: "rust".to_string(),
+        code: request.code,
+        status: RunStatus::Accepted,
+        stdout: None,
+        stderr: None,
+        exit_code: None,
+    };
+    let job_id = job.id;
+    let job_msg = JobMessage { job_id };
+    state.jobs.create(&job).await?;
+    state.kafka.send_job(&job_msg).await?;
+    Ok(Json(JobIdResponse { job_id }))
+}
+
+async fn get_job(
+    State(state): State<AppState>,
+    Path(job_id): Path<Uuid>,
+) -> Result<Json<JobResponse>, ApiError> {
+    let job = state.jobs.get(job_id).await?.ok_or(ApiError::JobNotFound)?;
+
+    Ok(Json(job.into()))
 }
