44921f35c0bf2db83625f2853b62f114381a3d30
ech0o <asdfghjklcx1250@outlook.com>
2026-08-17 20:41:06 +0800

    add: kafka,jobs

--- src/state.rs
+++ src/state.rs
@@ -1,18 +1,40 @@
+use std::collections::HashMap;
 use std::sync::Arc;
 
-use bollard::Docker;
-
 use crate::docker::DockerRunner;
+use crate::job::{Job, JobStore};
+use crate::kafka::KafkaProducer;
+use crate::models::{RunResponse, RunStatus};
+use bollard::Docker;
+use serde::{Deserialize, Serialize};
+use tokio::sync::{RwLock, Semaphore, mpsc};
+use uuid::Uuid;
+use crate::db::Database;
 
 #[derive(Clone)]
 pub struct AppState {
-    pub runner: DockerRunner,
+    // pub runner: DockerRunner,
+    pub semaphore: Arc<Semaphore>,
+    pub jobs: Arc<JobStore>,
+    pub kafka: KafkaProducer,
+    // pub db:Database
 }
 
 impl AppState {
-    pub fn new(docker: Docker) -> Self {
+    pub fn new(
+        job_store: Arc<JobStore>,
+        kafka_producer: KafkaProducer,
+    ) -> Self {
         Self {
-            runner: DockerRunner::new(Arc::new(docker)),
+            semaphore: Arc::new(Semaphore::new(4)),
+            jobs: job_store,
+            kafka: kafka_producer,
         }
     }
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct JobState {
+    pub status: RunStatus,
+    pub result: Option<RunResponse>,
 }
