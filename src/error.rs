daa02f16a6860732cb953aed1fa886eb79f71b8f
ech0o <asdfghjklcx1250@outlook.com>
2026-08-18 17:21:27 +0800

    add: ProcessError

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,18 @@
 use serde::Serialize;
-
-
+use crate::error::ProcessError::{Permanent, Retryable};
 
 pub struct ApiError(anyhow::Error);
 
+#[derive(thiserror::Error, Debug)]
+pub enum ProcessError {
+    #[error("retryable error: {0}")]
+    Retryable(anyhow::Error),
+    #[error("permanent error: {0}")]
+    Permanent(anyhow::Error),
+}
+
+// impl From<anyhow::Error> for ProcessError {
+//     fn from(e: anyhow::Error) -> Self {
+//         ProcessError::Permanent(e)
+//     }
+// }
