b68b07a702c29c590fccc3fda17c3e0d18942f3d
ech0o <asdfghjklcx1250@outlook.com>
2026-08-20 19:49:32 +0800

    chore:change to process

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ mod app;
 mod docker;
 mod routes;
 
-mod models;
-mod state;
-mod workspace;
 mod apierror;
+mod db;
 mod error;
 mod job;
 mod kafka;
-mod db;
+mod models;
+mod state;
+mod workspace;
 
 use tokio::net::TcpListener;
 
@@ -17,18 +17,13 @@ use tokio::net::TcpListener;
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     dotenvy::dotenv()?;
-    let app = app::create_app().await?;
-
-    let listener = TcpListener::bind("0.0.0.0:4000").await?;
-    tracing::info!("Listening on http://0.0.0.0:4000");
-
-    axum::serve(listener, app)
-        .with_graceful_shutdown(shutdown_signal())
-        .await?;
+    app::create_app().await?;
 
     Ok(())
 }
 
 async fn shutdown_signal() {
-    tokio::signal::ctrl_c().await.expect("failed to install CTRL+C signal handler");
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
 }
