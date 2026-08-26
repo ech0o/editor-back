e3bfd93476353d52fee7fd914732e5387d016f2f
ech0o <asdfghjklcx1250@outlook.com>
2026-08-26 10:08:59 +0800

    chore: change to worker id

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,4 +1,5 @@
-use prometheus::{Encoder, Gauge, Histogram, HistogramOpts, IntCounter, Registry};
+use prometheus::{Encoder, Gauge, Histogram, HistogramOpts, HistogramVec, IntCounter, IntCounterVec, Opts, Registry};
+use prometheus::core::Collector;
 
 pub fn gather() -> String {
     let metric_families = prometheus::gather();
@@ -9,23 +10,24 @@ pub fn gather() -> String {
 }
 
 pub struct RunningGuard {
-    gauge: Gauge,
+    gauge: IntCounter,
 }
 
 pub struct Metrics {
     pub worker_started_total: IntCounter,
     pub jobs_created_total: IntCounter,
-    pub jobs_finished_total: IntCounter,
+    pub jobs_finished_total: IntCounterVec,
     pub jobs_failed_total: IntCounter,
-    pub jobs_running: Gauge,
+    pub jobs_running: IntCounter,
 
-    pub job_duration_seconds: Histogram,
+    pub job_duration_seconds: HistogramVec,
     pub job_queue_latency_seconds: Histogram,
+    pub jobs_total:IntCounterVec,
     pub registry: Registry,
 }
 
 impl RunningGuard {
-    pub fn new(gauge: Gauge) -> Self {
+    pub fn new(gauge: IntCounter) -> Self {
         gauge.inc();
         Self { gauge }
     }
@@ -42,35 +44,45 @@ impl Metrics {
             "jobs_created_total",
             "Total number of jobs created",
         )?;
-        let jobs_finished_total=IntCounter::new(
-            "jobs_finished_total",
-            "Total number of jobs finished",
+        let jobs_finished_total=IntCounterVec::new(
+            Opts::new("jobs_finished_total", "Total number of jobs finished"),
+            &["worker_id"]
         )?;
         let jobs_failed_total=IntCounter::new(
             "jobs_failed_total",
             "Total number of jobs failed",
         )?;
 
-        let jobs_running=Gauge::new(
+        let jobs_running=IntCounter::new(
             "jobs_running",
             "Number of currently running jobs"
         )?;
 
-        let job_duration_seconds=Histogram::with_opts(HistogramOpts::new(
-            "job_duration_seconds",
-            "Job execution duration in seconds"
-        ))?;
+        let job_duration_seconds=HistogramVec::new(
+            HistogramOpts::new("job_duration_seconds", "Duration of job execution"),
+            &["worker_id"]
+        )?;
         let job_queue_latency_seconds=Histogram::with_opts(HistogramOpts::new(
             "job_queue_latency_seconds",
             "Time a job waits before being processed by a worker"
         ))?;
 
+        let jobs_total = IntCounterVec::new(
+            Opts::new(
+                "jobs_total",
+                "Total number of jobs processed",
+            ),
+            &["status"],
+        )?;
+
         registry.register(Box::new(worker_started_total.clone()))?;
+        registry.register(Box::new(jobs_running.clone()))?;
         registry.register(Box::new(jobs_created_total.clone()))?;
         registry.register(Box::new(jobs_finished_total.clone()))?;
         registry.register(Box::new(jobs_failed_total.clone()))?;
         registry.register(Box::new(job_duration_seconds.clone()))?;
         registry.register(Box::new(job_queue_latency_seconds.clone()))?;
+        registry.register(Box::new(jobs_total.clone()))?;
         Ok(Self{
             worker_started_total,
             jobs_created_total,
@@ -79,6 +91,7 @@ impl Metrics {
             jobs_running,
             job_duration_seconds,
             job_queue_latency_seconds,
+            jobs_total,
             registry,
         })
 
@@ -87,7 +100,7 @@ impl Metrics {
 
 impl Drop for RunningGuard {
     fn drop(&mut self) {
-        self.gauge.dec();
+        self.gauge.desc();
     }
 }
 
