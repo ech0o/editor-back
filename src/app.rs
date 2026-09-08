7ee2394c0cac381100febfbcbc24d152ba252dc7
ech0o <asdfghjklcx1250@outlook.com>
2026-09-08 08:54:41 +0800

    add: worker pool metrics

--- src/app.rs
+++ src/app.rs
@@ -88,7 +88,7 @@ async fn run_worker(
     //     ctx,
     //     shutdown.clone(),
     // )?;
-    let pool = Arc::new(WorkerPool::new(kafka_config, ctx, shutdown.clone()));
+    let pool = Arc::new(WorkerPool::new(kafka_config, ctx, shutdown.clone(),metrics.clone()));
     pool.start(3).await;
     pool.clone().start_reconciler().await;
     metrics.worker_started_total.inc();
